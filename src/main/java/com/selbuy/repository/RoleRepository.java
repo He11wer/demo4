58f2a0fbc214d42682059cc5ac66d6432a950222
He11wer <lalakalinana@gmail.com>
2025-05-29 13:37:10 +0400

    Update RoleRepository.java

--- src/main/java/com/selbuy/repository/RoleRepository.java
+++ src/main/java/com/selbuy/repository/RoleRepository.java
@@ -5,6 +5,7 @@ import com.selbuy.model.Role;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(String name);
 }
